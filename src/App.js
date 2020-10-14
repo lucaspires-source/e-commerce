88be2b597fe7b0fdce1827dae084a76915596bee
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-10-14 14:03:57 -0300

    bootstrap was installed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
+class App extends Component {
+  render (){
+  
   return (
-    <div className="App">
-      <h1>O pai ta on</h1>
+    <div className="container">
+    <div className="row">
+    <div className="col-6">Colum number 6</div>  
+    <div className="col-6">mambo number 5</div>
+    </div>  
     </div>
   );
+  }
 }
 
 export default App;
