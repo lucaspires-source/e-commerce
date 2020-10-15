e7487ce852733b16a27ba3680f8d60ec7eadeca1
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-10-15 15:45:37 -0300

    React Router installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
+import {Switch,Route} from "react-router-dom"
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar  from "./components/Navbar"
 import ProductList from "./components/ProductList"
-import Product from './components/Product';
 import Cart from "./components/Cart"
 import Details from "./components/Details"
 import Default from "./components/Default"
@@ -13,12 +13,12 @@ class App extends Component {
   return (
     <>
       <Navbar/>
-      <ProductList/>
-      <Product/>
-      <Cart/>
-      <Default/>
-      <Details/>
-
+      <Switch>
+          <Route exact path="/" component={ProductList}/>
+          <Route path="/details" component={Details}/>
+          <Route path="/cart" component={Cart}/>
+          <Route  component={Default}/>
+      </Switch>
     </>
 
   );
