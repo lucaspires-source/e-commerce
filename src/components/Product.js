87d7c4d2f302893a95d155661d8956dbac8df788
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-10-17 19:01:12 -0300

    small bug fixes

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,6 +4,9 @@ import {Link} from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 import {ProductConsumer} from "../context";
+import PropTypes from "prop-types"
+
+
 export default class Product extends Component {
     render() {
         const{id,title,img,price, inCart } = this.props.product
@@ -44,7 +47,15 @@ export default class Product extends Component {
     }
 }
 
-
+Product.propTypes = {
+    product:PropTypes.shape({
+        id:PropTypes.number,
+        img:PropTypes.string,
+        title:PropTypes.string,
+        price:PropTypes.number,
+        inCart:PropTypes.bool,
+    }).isRequired
+}
 const ProductWrapper = styled.div`
   .card {
     border-color: transparent;
