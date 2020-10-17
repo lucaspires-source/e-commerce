50ceaf75b0f2df27be4dba21b7e82c70b9052d88
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-10-17 18:14:23 -0300

    product component complete

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
 import Product from "./Product";
 import Title from "./Title";
-import {storeProducts} from "../data";
 import {ProductConsumer} from "../context";
-
+import styled from "styled-components";
 export default class ProductList extends Component {
 
-    state={
-        products:storeProducts
-    }
     render() {
-        console.log(this.state.products)
         return (
             <>
-                <div className="py-5">
-                    <div className="container">
-                        <Title name="our" title="products"/>
+                <ProductWrapper className="py-5">
+                    <Title name="our" title="products"/>
+                    <div className="container  d-flex justify-content-center flex-wrap">
                         <div className="row"/>
                         <ProductConsumer>
                             {value=>{
-                                return <h1>{value}</h1>
+                                return value.products.map(product =>{
+                                    return <Product key={product.id} 
+                                                    product={product}
+                                            />
+                                })
                             }}
                         </ProductConsumer>
                     </div>
-                </div>
+                </ProductWrapper>
             </>
 
         )
     }
 }
+
+const ProductWrapper = styled.section``
--- src/context.js
+++ src/context.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
-
+import {storeProducts, detailProduct} from "./data"
 
 const ProductContext = React.createContext();
 
 
 
 class ProductProvider extends Component {
+    state={
+        products:storeProducts,
+        detailProduct:detailProduct
+    }
+    handleDetail = () =>{
+        console.log("hello from detail")
+    }
+
+    addToCart = () =>{
+        console.log("hello from the cart")
+    }
+
     render() {
         return (
-            <ProductContext.Provider value="hello from context">
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleDetail:this.handleDetail,
+                addToCart:this.addToCart,
+            }}>
                 {this.props.children}
             </ProductContext.Provider>
         )
