745e408e7ac3ac4ac6a0ad2cd36c1811a7ac62a7
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-10-17 12:16:35 -0300

    button implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import logo from "../logo.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+import styled from "styled-components"
 export default class Navbar extends Component {
     render() {
         return (
@@ -18,12 +19,33 @@ export default class Navbar extends Component {
                     </l1>
                 </ul>
                 <Link to="/cart" className="ml-auto">
-                    <button>
-                       <FontAwesomeIcon icon={faCartPlus}/>
-                       my cart
-                    </button>
+                    <ButtonContainer>
+                        <span className="mr-2">
+                            <FontAwesomeIcon icon={faCartPlus}/>
+                        </span>
+                        my cart
+                    </ButtonContainer>
                 </Link>
             </nav>
         )
     }
 }
+const ButtonContainer = styled.button`
+    text-transform:capitalize;
+    font-size:1.4rem;
+    background:transparent;
+    border:0.05rem solid var(--lightgreen);
+    color:var(--lightgreen);
+    border-radius:0.5rem;
+    padding:0.2rem 0.5rem;
+    cursor:pointer;
+    margin:0.2rem 0.5rem 0.2rem 0;
+    transition:all 0.5s ease-in-out;
+    &:hover{
+        background:var(--lightgreen);
+        color:var(--mainGreen) ;
+    }
+    &:focus{
+        outline:none
+    }
+`;
