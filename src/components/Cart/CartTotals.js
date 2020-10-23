5952922da9a5466c1acb08131614cf5593287c21
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-10-23 12:33:53 -0300

    deploy settings

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 export default class CartTotals extends Component {
     render() {
-        const {cartSubTotal,cartTax,cartTotal,cart,clearCart} = this.props.value;
-        const { history } = this.props;
-        const emptyCart = cart.length === 0 ? true : false;
+        const {cartSubTotal,cartTax,cartTotal,clearCart} = this.props.value;
+        //const { history } = this.props;
+        //const emptyCart = cart.length === 0 ? true : false;
         return (
             <>
              <div className="container">
